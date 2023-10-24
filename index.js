6ae72f536776e938c386e033a981770c9bb88edd
System Administrator <root@pshreya-ltmv8fb.internal.salesforce.com>
2023-10-25 02:17:00 +0530

    commit-1

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
 const cool = require('cool-ascii-faces')
 const express = require('express')
 const path = require('path')
+const { Pool } = require('pg')
+
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false
+  }
+})
 
 const PORT = process.env.PORT || 5001
 
@@ -9,5 +17,28 @@ express()
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
   .get('/', (req, res) => res.render('pages/index'))
+  .get('/times', (req, res) => res.send(showTimes()))
   .get('/cool', (req, res) => res.send(cool()))
+  .get('/db', async (req, res) => {
+    try {
+      const client = await pool.connect();
+      const result = await client.query('SELECT * FROM test_table');
+      const results = { 'results': (result) ? result.rows : null};
+      res.render('pages/db', results );
+      client.release();
+    } catch (err) {
+      console.error(err);
+      res.send("Error " + err);
+    }
+  })
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+
+function showTimes() {
+  const times = process.env.TIMES || 5
+  let result = ''
+  for (i = 0; i < times; i++) {
+    result += i + ' '
+  }
+  return result
+}
+~
